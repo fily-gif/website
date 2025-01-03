4ab62afc9c457b10f1c0bc55ecbba3eefa770115
fily-jpg <zeroad69@gmail.com>
2025-01-02 21:27:55 -0500

    fsdf

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
-
+wsgi_app = app.wsgi_app
 if __name__ == '__main__':
     app.run(debug=False, port=5005)
